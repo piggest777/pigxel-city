1e7c25d043efce7027b7b2e7cbbd0959314d8cef
Денис Ракитин <piggest@mail.ru>
2018-05-14 02:58:00 +0300

    prepare collectionView

---
+++ pigxel-city/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  pigxel-city
+//
+//  Created by Denis Rakitin on 14/05/2018.
+//  Copyright © 2018 Denis Rakitin. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
